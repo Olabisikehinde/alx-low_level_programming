8b5a18e93e3d72af8394964892e558b2b6e16ff1
olabisikehinde <kennydinho75@gmail.com>
2023-09-11 11:52:20 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,19 +3,20 @@
  * main - this describes the main function of the code
  * Return: (0)
  */
-int main(void)
+int main(voi)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	int i;
+	float f;
+	char c;
+	long int g;
+	long long int e;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(g));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-	return (0)
+	return (0);
+
 }
